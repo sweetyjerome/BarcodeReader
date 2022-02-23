aade03dc4322c45f30e4a0801db24e1a753635f7
Sweety_Elizabeth Jerome <jerome@fim.uni-passau.de>
2022-02-23 10:45:57 +0100

    scanning with camera

--- ScannerApp/App.js
+++ ScannerApp/App.js
@@ -1,112 +1,114 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { StyleSheet, View, Alert } from 'react-native';
+import { RNCamera } from 'react-native-camera';
+import { useState } from 'react';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const [barcodes, setBarcodes] = useState([]);
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+  const barcodeRecognized = ({ barcodes }) => {
+    setBarcodes(barcodes)
+    if(barcodes){
+      barcodes.forEach(barcode => console.log(barcode.data));
+    }
+  }
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const renderBarcodes = () => (
+    <View>{barcodes.map(renderBarcode)}</View>
+  )
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const renderBarcode = ({ data }) => {
+    Alert.alert(
+      'Scanned Data',
+      data,
+      [
+        {
+          text: 'Okay',
+          onPress: () => console.log('Okay Pressed'),
+          style: 'cancel'
+        }
+      ],
+      { cancelable: false }
+    )
+  }
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+  return(
+    <View style={styles.container}>
+        <RNCamera
+          captureAudio = 'false'
+          ref={ref => {
+            camera = ref
+          }}
+          captureAudio={false}
+          type = {RNCamera.Constants.Type.back}
+          style={styles.scanner}
+          onGoogleVisionBarcodesDetected={barcodeRecognized}>
+          {barcodes? renderBarcodes : Alert.alert('error')}
+        </RNCamera>
+      </View>
+  )
+}
+
+
+// class App extends Component {
+//   state = {
+//     barcodes: [],
+//     scannedData: '',
+//   }
+  
+//   barcodeRecognized = ({ barcodes }) => {
+//     this.setState({ barcodes });
+//     if(barcodes){
+//       barcodes.forEach(barcode => console.log(barcode.data));
+//     }
+//   }
+//   renderBarcodes = () => (
+//     <View>{this.state.barcodes.map(this.renderBarcode)}</View>
+//   )
+
+//   renderBarcode = ({ data }) =>
+//     Alert.alert(
+//       'Scanned Data',
+//       data,
+//       [
+//         {
+//           text: 'Okay',
+//           onPress: () => console.log('Okay Pressed'),
+//           style: 'cancel'
+//         }
+//       ],
+//       { cancelable: false }
+//     )
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <RNCamera
+//           captureAudio = 'false'
+//           ref={ref => {
+//             this.camera = ref
+//           }}
+//           captureAudio={false}
+//           type = {RNCamera.Constants.Type.back}
+//           style={styles.scanner}
+//           onGoogleVisionBarcodesDetected={this.barcodeRecognized}>
+//           {this.state.barcodes? this.renderBarcodes : Alert.alert('error')}
+//         </RNCamera>
+//       </View>
+//     )
+//   }
+// }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    //backgroundColor: 'black'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  scanner: {
+    flex: 1,
+    // justifyContent: 'flex-end',
+    // alignItems: 'center'
+  }
+})
 
-export default App;
+export default App
