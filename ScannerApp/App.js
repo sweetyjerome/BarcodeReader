8190ed7130ac1d1bb0961522f725c8602054e2cc
Sweety_Elizabeth Jerome <jerome@fim.uni-passau.de>
2022-02-23 18:24:28 +0100

    scanning and api call in single component

--- ScannerApp/App.js
+++ ScannerApp/App.js
@@ -1,39 +1,82 @@
 import React from 'react';
 import { StyleSheet, View, Alert } from 'react-native';
 import { RNCamera } from 'react-native-camera';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Axios from 'axios';
+import config from './errors.config';
 
 const App = () => {
   const [barcodes, setBarcodes] = useState([]);
   const [scannedData, setScannedData] = useState('');
-  const userId= 400000000
-  const item = 978020137962
-
+  const [item, setItem] = useState('');
+  const errorMessages = config.errors;
+   
   useEffect(() => {
+    let userId = 400000000;
     Axios.get(`http://opengtindb.org/?ean=${item}&cmd=query&queryid=${userId}`)
     .then(res => {
       console.log(res)
+      setScannedData(res.data)
     }).catch(err => {
       console.log(err);
     })
-  }, []);
+  }, [item]);
 
   const barcodeRecognized = ({ barcodes }) => {
     setBarcodes(barcodes)
-    if(barcodes){
-      barcodes.forEach(barcode => console.log(barcode.data));
-    }
   }
 
   const renderBarcodes = () => (
     <View>{barcodes.map(renderBarcode)}</View>
   )
-
   const renderBarcode = ({ data }) => {
+    setItem(data)
+    parsedResponse()
+  }
+  const parsedResponse = () => {
+    let lines = scannedData.split(/\r\n|\r|\n/);
+    let paramRegex = /^\s*([\w\.\-\_]+)\s*=\s*(.*?)\s*$/,
+        cnt = 0, //index of the product count. (0= first product being scanned)
+        retVal = {
+            error : false,
+            data : []
+        };
+
+    lines.forEach(line => {
+        console.log('line',line)
+         
+        if(paramRegex.test(line)) {
+            let match = line.match(paramRegex),
+            currentProduct = retVal.data[cnt];
+            console.log('mmmm',match)
+            console.log('nnnn',currentProduct)
+            switch(match[1]) {
+                case "error":
+                    retVal.error = match[2];
+                    break;
+                case "name":
+                    currentProduct['name'] = match[2];
+                    break;
+                default:
+                    retVal.data[cnt][match[1]] = match[2];
+                    break;
+            }
+        }
+        else {
+            // add a new product
+            if(line.indexOf('---') === 0) {
+                cnt++;
+                retVal.data[cnt] = {};
+            }
+        }
+    });
+    if(parseInt(retVal.error) > 3) {
+        console.log(retVal.error);
+        throw new Error(errorMessages[parseInt(retVal.error,10)].msg);
+    }
     Alert.alert(
       'Scanned Data',
-      data,
+      retVal.data[1].name,
       [
         {
           text: 'Okay',
@@ -43,7 +86,9 @@ const App = () => {
       ],
       { cancelable: false }
     )
-  }
+    console.log('dataaaa',retVal) //prints an object with a data array which has the name and details of the product as an object 
+    
+} 
 
   return(
     <View style={styles.container}>
@@ -62,65 +107,13 @@ const App = () => {
   )
 }
 
-
-// class App extends Component {
-//   state = {
-//     barcodes: [],
-//     scannedData: '',
-//   }
-  
-//   barcodeRecognized = ({ barcodes }) => {
-//     this.setState({ barcodes });
-//     if(barcodes){
-//       barcodes.forEach(barcode => console.log(barcode.data));
-//     }
-//   }
-//   renderBarcodes = () => (
-//     <View>{this.state.barcodes.map(this.renderBarcode)}</View>
-//   )
-
-//   renderBarcode = ({ data }) =>
-//     Alert.alert(
-//       'Scanned Data',
-//       data,
-//       [
-//         {
-//           text: 'Okay',
-//           onPress: () => console.log('Okay Pressed'),
-//           style: 'cancel'
-//         }
-//       ],
-//       { cancelable: false }
-//     )
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <RNCamera
-//           captureAudio = 'false'
-//           ref={ref => {
-//             this.camera = ref
-//           }}
-//           captureAudio={false}
-//           type = {RNCamera.Constants.Type.back}
-//           style={styles.scanner}
-//           onGoogleVisionBarcodesDetected={this.barcodeRecognized}>
-//           {this.state.barcodes? this.renderBarcodes : Alert.alert('error')}
-//         </RNCamera>
-//       </View>
-//     )
-//   }
-// }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    //backgroundColor: 'black'
   },
   scanner: {
     flex: 1,
-    // justifyContent: 'flex-end',
-    // alignItems: 'center'
   }
 })
 
--- ScannerApp/index.js
+++ ScannerApp/index.js
@@ -5,6 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import Sample from './sample';
+// import Sample from './sample';
 
-AppRegistry.registerComponent(appName, () => Sample);
+AppRegistry.registerComponent(appName, () => App);
